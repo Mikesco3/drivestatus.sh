f8a8b15ce4d0029aec3591566b9f5a635f20ec06
Mikesco3 <mikesco3@gmail.com>
2024-06-30 00:12:15 -0500

    Update drivestatus.sh
    Optimized the script to avoid creating temporary files.

--- drivestatus.sh
+++ drivestatus.sh
@@ -2,25 +2,25 @@
 
 ## Inspired from:  https://www.youtube.com/watch?v=1YGt5o35mo0
 
-# Get the list of drives, ommiting zfs datasets
+# Get the list of drives
 drives=$(lsblk | grep disk | grep -v zd | awk '{print "/dev/" $1}')
 
 for drive in $drives
 do
-  smartctl -H $drive > /tmp/asdasd
-  if grep -q "No such device" /tmp/asdasd; then
-    rm -f /tmp/asdasd
-  else
-    rm -f /tmp/asdasd
-    smartctl -H $drive > /tmp/assessmentstatus
-    if grep -q -i -e passed -e "SMART Health Status: OK" /tmp/assessmentstatus; then
-      echo "$drive  : GOOD"
-    elif grep -q -i failed /tmp/assessmentstatus; then
-      echo "$drive  : $(tput setaf 1)REPLACE$(tput sgr 0)"
-    else
-      echo "$drive  : UNKNOWN STATUS"
-    fi
-    rm -f /tmp/assessmentstatus
+  # Run smartctl command and capture the output
+  output=$(smartctl -H $drive 2>&1)
+  
+  if echo "$output" | grep -q "No such device"; then
+    continue
   fi
+
+  if echo "$output" | grep -qi -e passed -e "SMART Health Status: OK"; then
+    echo "$drive  : GOOD"
+  elif echo "$output" | grep -qi failed; then
+    echo "$drive  : $(tput setaf 1)REPLACE$(tput sgr 0)"
+  else
+    echo "$drive  : UNKNOWN STATUS"
+  fi
+
 done
 
